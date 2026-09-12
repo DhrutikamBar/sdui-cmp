09cffb4f04468a8685b2bb86541cf3585d914884
Dhrutikam Barik <55571944+DhrutikamBar@users.noreply.github.com>
2026-09-12 21:49:23 +0530

    feat(sdk): expose action dispatch outcomes

--- sdui-sdk/src/commonMain/kotlin/com/dhruti/sdui/sdk/ActionRegistry.kt
+++ sdui-sdk/src/commonMain/kotlin/com/dhruti/sdui/sdk/ActionRegistry.kt
@@ -10,6 +10,14 @@ interface ActionInterceptor {
     fun intercept(action: UiAction, next: (UiAction) -> Unit)
 }
 
+/** Outcome of synchronous action dispatch. Asynchronous hosts can use this to
+ * report that a request was accepted, then report their own completion outcome. */
+sealed interface SduiActionDispatchResult {
+    data object Dispatched : SduiActionDispatchResult
+    data class Denied(val reason: String) : SduiActionDispatchResult
+    data object Unhandled : SduiActionDispatchResult
+}
+
 /**
  * Registry for action handlers and interceptors.
  */
@@ -23,12 +31,12 @@ class ActionRegistry(
         handlers[type] = handler
     }
 
-    fun dispatch(action: UiAction) {
+    fun dispatch(action: UiAction): SduiActionDispatchResult {
         when (val decision = actionPolicy.evaluate(action)) {
             SduiActionDecision.Allow -> Unit
             is SduiActionDecision.Deny -> {
                 println("Denied action type: ${action.type}. Reason: ${decision.reason}")
-                return
+                return SduiActionDispatchResult.Denied(decision.reason)
             }
         }
 
@@ -48,8 +56,10 @@ class ActionRegistry(
                 }
             }
         }
-        
+
+        val hasHandler = handlers.containsKey(action.type)
         next(action)
+        return if (hasHandler) SduiActionDispatchResult.Dispatched else SduiActionDispatchResult.Unhandled
     }
 }
 
