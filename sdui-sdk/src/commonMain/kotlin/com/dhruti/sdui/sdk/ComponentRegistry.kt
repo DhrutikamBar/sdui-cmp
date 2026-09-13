455b6721ce1ab0c8fc9fed4bf826c7524d55b352
Dhrutikam Barik <55571944+DhrutikamBar@users.noreply.github.com>
2026-09-13 09:19:25 +0530

    fix(sdk): import navigation bar inset extension

--- sdui-sdk/src/commonMain/kotlin/com/dhruti/sdui/sdk/ComponentRegistry.kt
+++ sdui-sdk/src/commonMain/kotlin/com/dhruti/sdui/sdk/ComponentRegistry.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
